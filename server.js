b7a7c492a7c78838617e0b7142f764ea4db6bec6
rabidima <dmitrirabinovich@Dmitris-MacBook-Pro.local>
2016-11-19 19:58:11 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+// ==============================================================================
+// DEPENDENCIES
+// ==============================================================================
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// ==============================================================================
+
+var app = express(); // create an "express" server
+var PORT = process.env.PORT || 3000; // Sets an initial port.
+
+// BodyParser makes it easy for our server to interpret data sent to it.
+app.use(bodyParser.json());	
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+
+// ================================================================================
+// ROUTER
+// ================================================================================
+
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
+
+
+// ==============================================================================
+// LISTENER
+// ==============================================================================
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
