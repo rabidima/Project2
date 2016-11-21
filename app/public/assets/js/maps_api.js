dcf608bf27560792d4f476917ae661d5f11057ce
rabidima <dmitrirabinovich@Dmitris-MacBook-Pro.local>
2016-11-20 23:36:55 -0800

    Uppdated map

--- app/public/assets/js/maps_api.js
+++ app/public/assets/js/maps_api.js
@@ -1,8 +1,11 @@
 
 $(document).ready(function() {
 
+ 
 function getZip() {
 	// $('#apiBtn').on('click', function() {
+
+    
 		var zipcode = $('#zipInput').val().trim();
 		var queryURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&sensor=false`
 		console.log('Location entered:', zipcode);
@@ -42,7 +45,7 @@ function getZip() {
       .done(function(trailsList) {
         var trails = JSON.parse(trailsList);
 
-        initMap(trails); 
+        initialize(trails); 
 
         // Loop through each of the trails 
         for (var i=0; i<trails.length; i++){
@@ -64,6 +67,67 @@ function getZip() {
           
         });
   }
+
+
+      
+    function initialize(trails) {
+
+        var map;
+        var bounds = new google.maps.LatLngBounds();
+        var mapOptions = {
+            mapTypeId: 'roadmap'
+        };
+                        
+        // Display a map on the page
+        map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        map.setTilt(45);
+            
+                          
+        // Info Window Content
+        var infoWindowContent = [
+            ['<div class="info_content">' +
+            '<h3>London Eye</h3>' +
+            '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' +        '</div>'],
+            ['<div class="info_content">' +
+            '<h3>Palace of Westminster</h3>' +
+            '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
+            '</div>']
+        ];
+          
+        var marker = [];
+        // Display multiple markers on a map
+        var infoWindow = new google.maps.InfoWindow(), marker, i;        
+      
+        // Loop through our array of markers & place each one on the map  
+        for( i = 0; i < trails.length; i++ ) {
+            var position = new google.maps.LatLng(trails[i].latitude, trails[i].longitude);
+            bounds.extend(position);
+            marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                title: trails[i].name
+            });
+            
+            // Allow each marker to have an info window    
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infoWindow.setContent(infoWindowContent[i][0]);
+                    infoWindow.open(map, marker);
+                }
+            })(marker, i));
+
+            // Automatically center the map fitting all markers on the screen
+            map.fitBounds(bounds);
+        }
+
+        // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+        var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+            this.setZoom(10);
+            google.maps.event.removeListener(boundsListener);
+        });
+        
+    }
+     
 
 	// Geolocation: Display and Update Google Map based on search location
 	function initAutocomplete() {
@@ -112,7 +176,7 @@ function getZip() {
           searchBox.setBounds(map.getBounds());
         });
 
-        var markers = [];
+        // var markers = [];
         // Listen for the event fired when the user selects a prediction and retrieve
         // more details for that place.
         searchBox.addListener('places_changed', function() {
@@ -122,65 +186,48 @@ function getZip() {
             return;
           }
 
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
-          markers = [];
+        //   // Clear out the old markers.
+        //   markers.forEach(function(marker) {
+        //     marker.setMap(null);
+        //   });
+        //   // markers = [];
 
-          // For each place, get the icon, name and location.
+        //   // For each place, get the icon, name and location.
           var bounds = new google.maps.LatLngBounds();
           places.forEach(function(place) {
             if (!place.geometry) {
               console.log("Returned place contains no geometry");
               return;
             }
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
+            // var icon = {
+        //       url: place.icon,
+        //       size: new google.maps.Size(71, 71),
+        //       origin: new google.maps.Point(0, 0),
+        //       anchor: new google.maps.Point(17, 34),
+        //       scaledSize: new google.maps.Size(25, 25)
+        //     };
 
-            // Create a marker 
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
+        //     // Create a marker 
+        //     markers.push(new google.maps.Marker({
+        //       map: map,
+        //       icon: icon,
+        //       title: place.name,
+        //       position: place.geometry.location
+        //     }));
 
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
+            // if (place.geometry.viewport) {
+            //   // Only geocodes have viewport.
+            //   bounds.union(place.geometry.viewport);
+            // } else {
+            //   bounds.extend(place.geometry.location);
+            // }
           });
           map.fitBounds(bounds);
           getZip();
         });
       }
+
 	initAutocomplete();
-
-// Place markers
-  function initMap(trails) {
-    // Create a marker 
-    var markers = [];
-    var map = new google.maps.Map(document.getElementById('map'), {
-        
-      });
-    for (var i=0; i<trails.length; i++){
-      var uluru = {lat: trails[i].latitude, lng: trails[i].longitude};
-      
-      markers.push(new google.maps.Marker({
-        position: uluru,
-        map: map
-      }));
-    } 
-  }
-
 
 
 
